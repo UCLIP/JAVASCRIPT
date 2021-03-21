f08b60df31a5235ccb6bc1b916a00b63ee36badf
UCLIP <75576763+UCLIP@users.noreply.github.com>
2021-03-21 22:13:57 +0100

    Add files via upload

---
+++ IA/script1.js
@@ -0,0 +1,21 @@
+document.getElementById("perimeter").onclick = function () {
+    location.href = "Perimeter/index.html";
+};
+document.getElementById("IA").onclick = function () {
+    location.href = "IA/index.html";
+};
+document.getElementById("consequences").onclick = function () {
+    location.href = "consequences/index.html";
+};
+setTimeout(couleur, 4000);
+function couleur()
+{
+    color=prompt("entrez la couleur que vous voulez en anglais pour le site ");
+    document.body.style.backgroundColor =color;
+}
+
+function utilisateurinteract2(){
+
+
+
+}
